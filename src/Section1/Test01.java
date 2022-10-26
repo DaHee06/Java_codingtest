6954fb4a2a58918c4638e41913524cd666095042
DaHee06 <todayda1006@gmail.com>
2022-10-26 22:30:32 +0900

    코딩 테스트 1-02

--- src/Section1/Test01.java
+++ src/Section1/Test01.java
@@ -4,9 +4,30 @@ import java.util.Scanner;
 
 //한 개의 문자열을 입력받고, 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에 몇 개 존재하는지 알아내는 프로그램을 작성하세요.
 public class Test01 {
+
+    public int solution(String str, char c){
+        int answer=0 ;
+        String s = str.toUpperCase();
+        c = Character.toUpperCase(c);
+
+        //문자 배열로 만들어서 진행
+//        char[] a = s.toCharArray();
+//        for(char b : a){
+//            if(c==b) answer++;
+//        }
+
+        //변환 없이 진행
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==c) answer++;
+        }
+        return answer;
+    }
+
     public static void main(String[] args) {
-        int n = 100;
-//        String str = String.valueOf(n);
-        System.out.println("12"+n);
+        Test01 t = new Test01();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char c = sc.next().charAt(0);
+        System.out.println(t.solution(str,c));
     }
 }
--- src/Section1/Test02.java
+++ src/Section1/Test02.java
@@ -5,5 +5,26 @@ import java.util.Scanner;
 //대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로 소문자는 대문자로 변환하여 출력하는 프로그램을 작성하세요
 public class Test02 {
 
+    public String solution(String str){
+        String answer="";
+//        for(char x : str.toCharArray()){
+//            if(Character.isLowerCase(x)) answer += Character.toUpperCase(x);
+//            else answer += Character.toLowerCase(x);
+//        }
+
+        //아스키 번호 이용
+        for(char x : str.toCharArray()){
+            if(x>=97 && x<=122) answer += (char)(x-32);
+            else answer += (char)(x+32);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Test02 t = new Test02();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(t.solution(str));
+    }
 
 }
