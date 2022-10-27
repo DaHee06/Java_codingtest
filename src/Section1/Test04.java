80c0aefa59ffeceabc439d3aa01b4288ef044183
DaHee06 <todayda1006@gmail.com>
2022-10-27 10:36:59 +0900

    코딩 테스트 1-03

--- src/Section1/Test04.java
+++ src/Section1/Test04.java
@@ -2,4 +2,9 @@ package Section1;
 
 //N개의 단어가 주어지면 각 단어를 뒤집어 출력하는 프로그램을 작성하세요
 public class Test04 {
+    public static void main(String[] args) {
+        String str = "it is time to study";
+        String[] s = str.split(" ");
+        for(String a : s) System.out.println(a);
+    }
 }
